171d13ac30a4f05771d811f105703e9613b45e4b
Junko Tsujimoto <junko.tsujimoto@gmail.com>
2013-06-25 13:06:52 +0900

    追加：クラウドシティニュースのタブ表示と全体表示の切り替え
    クラウドシティニュースをタブ表示（デフォルト）と全体表示の切り替えができるようにする。

--- www/js/navi.js
+++ www/js/navi.js
@@ -3,7 +3,8 @@ var selectedTabID = 'nav_01';
 Event.observe(window, 'load', function(){
 	var tabs = document.getElementById('navigation').getElementsByTagName('li');
 	var tabNums = tabs.length;
-
+	var cContentsStr = "";
+	
 	for(var i = 0; i < tabNums ; i++) {
 		tabID = 'nav_0'+(i+1);
 		Event.observe(tabID, 'click',function(){tabSelect(this.id);});
@@ -11,7 +12,13 @@ Event.observe(window, 'load', function(){
 		if (Element.hasClassName(item, 'selected')) {
 			tabSelect(tabID);
 		}
+		cID = tabID + '_c';
+		cContentsStr += "<hr>" + $(cID).innerHTML;
 	}
+	Element.update($("nav_all_contents"),cContentsStr);
+	setDate();
+	var is_tab = ($F('c_ccn_default_tab') == 1) ? true : false;
+	setTab(is_tab);
 });
 
 
@@ -34,4 +41,23 @@ function tabSelect(id){
 
 	//タブ切り替え
 	selectedTabID = clickedTabID;
+}
+
+function setTab(flg){
+	if(flg == true) {
+		Element.setStyle($('nav'), {'display':'block'});
+		Element.hide($('nav_all'));
+	}
+	else{
+		Element.setStyle($('nav_all'), {'display':'block'});
+		Element.hide($('nav'))
+	}
+	return 0;
+}
+
+function setDate(){
+	var nowdate = new Date();
+	var mon = nowdate.getMonth() + 1;
+	var date = nowdate.getDate();
+	Element.update($('date_of_today'), mon + "月" + date + "日");
 }
